4e400e3d7a99ea06e8703b77ea51d65fbfb2805e
Simon Tower <simon@klein-homepage.de>
2022-06-02 13:09:57 +0200

    changed name of Ether to the native currency and corrected chain ID in one place

--- src/connectors/index.ts
+++ src/connectors/index.ts
@@ -26,7 +26,7 @@ export function getNetworkLibrary(): Web3Provider {
 }
 
 export const injected = new InjectedConnector({
-  supportedChainIds: [1, 4],
+  supportedChainIds: [32520],
 });
 
 // mainnet only
