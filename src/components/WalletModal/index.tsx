6c315ef519db8a1a03646ea3478e900fffc3684b
Simon Tower <simon@klein-homepage.de>
2022-10-08 16:29:51 +0200

    added ARCO for Bitgert

--- src/components/WalletModal/index.tsx
+++ src/components/WalletModal/index.tsx
@@ -289,7 +289,7 @@ export default function WalletModal({
           <HeaderRow>{error instanceof UnsupportedChainIdError ? 'Wrong Network' : 'Error connecting'}</HeaderRow>
           <ContentWrapper>
             {error instanceof UnsupportedChainIdError ? (
-              <h5>Please connect to Bitgert or Dogechain.</h5>
+              <h5>Please connect to Bitgert, Dogechain or Dokenchain.</h5>
             ) : (
               'Error connecting. Try refreshing the page.'
             )}
